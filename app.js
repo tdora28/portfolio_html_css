cba950f97d5a44c2ca48c2b9fe0dc0982ceb875e
Dóra Tokai <tokaidora88@gmail.com>
2023-10-13 13:40:26 +0300

    Correct scroll function visibility toggle on header and back-to-top btn

--- app.js
+++ app.js
@@ -1,65 +1,46 @@
-const header = document.querySelector('#header');
-const backToTopBtn = document.querySelector('#back-to-top');
-const openMenuBtn = document.querySelector('#openMobileMenuBtn');
-const closeMenuBtn = document.querySelector('#closeMobileMenuBtn');
-const navLinks = document.querySelectorAll('.nav a');
+const header = document.querySelector("#header");
+const backToTopBtn = document.querySelector("#back-to-top");
+const openMenuBtn = document.querySelector("#openMobileMenuBtn");
+const closeMenuBtn = document.querySelector("#closeMobileMenuBtn");
+const navLinks = document.querySelectorAll(".nav a");
 
-const MOBILE_UPPER_SIZE = 601;
+backToTopBtn.style.display = "none";
+
+window.onscroll = function () {
+  scrollFunction();
+};
 
 // Header background-color changes on scroll and mouse events
-let timer = null;
-window.addEventListener('scroll', () => {
-  // Cancel for small screens
-  if (window.innerWidth < MOBILE_UPPER_SIZE) {
-    return;
-  }
-  // Make background solid for a short time on scroll
-  if (timer !== null) {
-    clearTimeout(timer);
-    header.style.background = 'var(--clr-bg)';
-  }
-  timer = setTimeout(() => {
-    header.style.background = 'var(--clr-bg-transparent)';
-  }, 400);
-});
+// Toggling bakc-to-top btn display
+// Code from W3S
+const scrollFunction = () => {
+  // Hiding back to top btn on scroll
+  backToTopBtn.style.display = document.body.scrollTop > 200 || document.documentElement.scrollTop > 200 ? "flex" : "none";
 
-header.addEventListener('mouseover', () => {
-  // Cancel for small screens
-  if (window.innerWidth < MOBILE_UPPER_SIZE) {
-    return;
-  }
-  header.style.background = 'var(--clr-bg)';
-});
-
-header.addEventListener('mouseout', () => {
-  // Cancel for small screens
-  if (window.innerWidth < MOBILE_UPPER_SIZE) {
-    return;
-  }
-  header.style.background = 'var(--clr-bg-transparent)';
-});
+  // Show header bg on scroll
+  document.body.scrollTop > 50 || document.documentElement.scrollTop > 50 ? header.classList.add("background") : header.classList.remove("background");
+};
 
 // Back to top functionality
-backToTopBtn.addEventListener('click', () => {
+backToTopBtn.addEventListener("click", () => {
   window.scrollTo(0, 0);
 });
 
 // Mobile menu
 const openMenu = () => {
-  openMenuBtn.classList.add('hidden');
-  header.classList.add('visible');
+  openMenuBtn.classList.add("hidden");
+  header.classList.add("visible");
 };
 
 const closeMenu = () => {
-  openMenuBtn.classList.remove('hidden');
-  header.classList.remove('visible');
-  header.style.backgroundColor = 'white';
+  openMenuBtn.classList.remove("hidden");
+  header.classList.remove("visible");
 };
 
-openMenuBtn.addEventListener('click', openMenu);
+openMenuBtn.addEventListener("click", openMenu);
 
-closeMenuBtn.addEventListener('click', closeMenu);
+closeMenuBtn.addEventListener("click", closeMenu);
 
 navLinks.forEach((link) => {
-  link.addEventListener('click', closeMenu);
+  link.addEventListener("click", closeMenu);
 });
